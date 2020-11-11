9dab25513ea920c4dcadfde2c4bfba5285480f78
78Alpha <37676834+78Alpha@users.noreply.github.com>
2020-11-11 08:16:19 -0800

    Fix Bin filtering to match FBA

--- Applications/BinFilterOPT.py
+++ Applications/BinFilterOPT.py
@@ -3,7 +3,7 @@ import re
 _C_BIN_FORMAT_REGEX_ = re.compile("^P-[0-9]-[A-Z]{1}[0-9]{3}[A-Z]{1}[0-9]{3}$")
 _C_POD_ID_REGEX_ = re.compile("^HB05[0-9]{9}")
 _C_BIN_LABEL_REGEX_ = re.compile("^[0-9]{1}[A-Z]{1}")
-_C_SPLIT_BY_: list = ["\t", " \t"]
+_C_SPLIT_BY_: list = [" \t", "\t"]
 _C_FACE_REGEX_ = re.compile("^[A-Z]{1}")
 
 
@@ -41,4 +41,3 @@ def userInputAsBin(initial, binRegex=_C_BIN_FORMAT_REGEX_):
 def getFaces(initial, faceRegex=_C_FACE_REGEX_):
     faceArray = [face for face in initial if faceRegex.match(face) and len(face) == 1]
     return faceArray
-
